c172a12ee4427056f0ee2feabd405087ab5d3b37
Catalina <maria.catalina15082004@gmail.com>
2024-06-16 14:22:52 +0300

    session_4_java_operators_and_loops_challenges

---
+++ src/main/java/session_4_java_operators_and_loops_challenges/Problem3.java
@@ -0,0 +1,15 @@
+package session_4_java_operators_and_loops_challenges;
+
+public class Problem3 {
+
+    public static void main(String[] args) {
+        /* 3. Write a Java program that declares an integer variable age with a value of 20.
+         Print a statement saying "I am adult" if age is 18 or more, else print "I am not an adult". */
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("I am an adult. ");
+        } else {
+            System.out.println("I am not an adult. ");
+        }
+    }
+}
