c702b73c38d9aa94a89f3f6e1e141171f56d80c1
Catalina <maria.catalina15082004@gmail.com>
2024-07-23 00:04:05 +0300

    session_15_oop_recap_practice&challenge

---
+++ src/main/java/session_15_oop_recap/practice/GameCharacter.java
@@ -0,0 +1,18 @@
+package session_15_oop_recap.practice;
+
+public class GameCharacter {
+
+    private Weapon weapon;
+
+    public GameCharacter(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    void attack() {
+        weapon.use();
+    }
+}
---
+++ src/main/java/session_15_oop_recap/practice/Sword.java
@@ -0,0 +1,9 @@
+package session_15_oop_recap.practice;
+
+public class Sword implements Weapon {
+
+    @Override
+    public void use() {
+        System.out.println("Using sword");
+    }
+}
